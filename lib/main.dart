ea7c7aa6c3fbd93834f96049ffc816c67c7068b0
bayazid_sustami <bayazidsustamy@gmail.com>
2021-06-14 04:41:21 +0800

    add safe area for padding depend on device

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,49 +6,26 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: FirstScreen(),
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: DetailScreen(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget{
+class DetailScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading:IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.white,
-          ), onPressed: () {  },
-        ) ,
-        title: Text('First Screen'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.white,
-            ), onPressed: () {  },
-          )
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          Text('a title', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-          Text('Lorem ipsum ipsum'),
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Text('text in border'),
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.red
-            ),
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+                margin: EdgeInsets.only(top: 16.0),
+                child: Text('Farm House Lembang')
+            )
+          ],
+        ),
       )
     );
   }
