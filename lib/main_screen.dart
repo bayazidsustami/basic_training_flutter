00208dbb659acfcd8ce13a796547412b5238e5e1
bayazid_sustami <bayazidsustamy@gmail.com>
2021-06-16 01:26:01 +0800

    add item list in main

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learn/detail_screen.dart';
+import 'package:flutter_learn/model/tourism_place.dart';
 
 class MainScreen extends StatelessWidget{
   @override
@@ -8,41 +9,46 @@ class MainScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text('Wisata Bandung'),
       ),
-      body: InkWell(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen()));
-        },
-        child: Card(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Image.asset('images/farm-house.jpg'),
-              ),
-              Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        'Farm House Lembang',
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                      SizedBox(
-                        height: 80,
-                      ),
-                      Text('Lembang')
-                    ],
-                  ),
+      body: ListView.builder(itemBuilder: (context, index){
+        final TourismPlace place = tourismPlaceList[index];
+        return InkWell(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen()));
+          },
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Image.asset(place.imageAsset),
                 ),
-              )
-            ],
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          place.name,
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Text(place.location)
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
+        );
+      },
+        itemCount: tourismPlaceList.length,
       )
     );
   }
