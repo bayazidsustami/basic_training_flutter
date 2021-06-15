1cba3bd8d0035306e68b8811b7d59931946e31c2
bayazid_sustami <bayazidsustamy@gmail.com>
2021-06-16 00:46:06 +0800

    responsive ui using layout builder

--- lib/main.dart
+++ lib/main.dart
@@ -16,57 +16,39 @@ class MyApp extends StatelessWidget {
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Row(
-          children: [
-            Expanded(child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        'MediaQuery: ${screenSize.width.toStringAsFixed(2)}',
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                        textAlign: TextAlign.center,
-                      ),
-                      Text(
-                        'LayoutBuilder: ${constraints.maxWidth}',
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                        textAlign: TextAlign.center,
-                      ),
-                    ]);
-              },
-            )),
-            Expanded(
-              flex: 3,
-              child: LayoutBuilder(
-                  builder: (BuildContext context, BoxConstraints constraints) {
-                return Container(
-                    color: Colors.white,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(
-                            'MediaQuery: ${screenSize.width.toStringAsFixed(2)}',
-                            style:
-                                TextStyle(color: Colors.blueGrey, fontSize: 18),
-                            textAlign: TextAlign.center,
-                          ),
-                          Text(
-                            'LayoutBuilder: ${constraints.maxWidth}',
-                            style:
-                                TextStyle(color: Colors.blueGrey, fontSize: 18),
-                            textAlign: TextAlign.center,
-                          ),
-                        ]));
-              }),
-            )
-          ],
-        ));
+        appBar: AppBar(),
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints){
+            if(constraints.maxWidth < 600){
+              return ListView(
+                children: _generateContainer(),
+              );
+            }else if(constraints.maxWidth < 900){
+              return GridView.count(
+                  crossAxisCount: 2,
+                children: _generateContainer(),
+              );
+            } else {
+              return GridView.count(
+                crossAxisCount: 6,
+                children: _generateContainer(),
+              );
+            }
+          },
+        )
+    );
+  }
+
+  List<Widget> _generateContainer() {
+    return List<Widget>.generate(20, (index) {
+      return Container(
+        margin: const EdgeInsets.all(10),
+        color: Colors.blueGrey,
+        height: 200,
+      );
+    });
   }
 }
+
