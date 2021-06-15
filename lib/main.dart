f1f0f72e0a93eb2b27f0bf66d17e66b48309b461
bayazid_sustami <bayazidsustamy@gmail.com>
2021-06-15 23:31:17 +0800

    using listView from array

--- lib/main.dart
+++ lib/main.dart
@@ -16,64 +16,28 @@ class MyApp extends StatelessWidget{
 }
 
 class ScrollingScreen extends StatelessWidget{
+
+  final List<int> numberList = <int>[1,2,3,4,5,6,7,8,9,10];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: <Widget>[
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black)
-            ),
-            child: Center(
-              child: Text(
-                '1',
-                style: TextStyle(fontSize: 50),
+        children: numberList.map((e) =>
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                  color: Colors.grey,
+                  border: Border.all(color: Colors.black)
               ),
-            ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black)
-            ),
-            child: Center(
-              child: Text(
-                '2',
-                style: TextStyle(fontSize: 50),
+              child: Center(
+                child: Text(
+                  '$e',
+                  style: TextStyle(fontSize: 50),
+                ),
               ),
-            ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black)
-            ),
-            child: Center(
-              child: Text(
-                '3',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-                color: Colors.grey,
-                border: Border.all(color: Colors.black)
-            ),
-            child: Center(
-              child: Text(
-                '4',
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          )
-        ],
+            )
+        ).toList(),
       ),
     );
   }
