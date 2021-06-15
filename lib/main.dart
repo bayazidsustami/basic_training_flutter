942f126eadc3ee764e4d3e8b1fb19f9617c15fe6
bayazid_sustami <bayazidsustamy@gmail.com>
2021-06-15 22:08:04 +0800

    add input using checkBox

--- lib/main.dart
+++ lib/main.dart
@@ -23,67 +23,26 @@ class FirstScreen extends StatefulWidget{
 
 class _FirstScreen extends State<FirstScreen>{
 
-  String language;
+  bool isAgree = false;
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('First Screen'),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: Radio<String>(
-              value: 'Dart',
-              groupValue: language,
-              onChanged: (String value){
-                setState(() {
-                  language = value;
-                  showSnackBar();
-                });
-              },
-            ),
-            title: (Text('Dart')),
-          ),
-          ListTile(
-            leading: Radio<String>(
-              value: 'Kotlin',
-              groupValue: language,
-              onChanged: (String value){
-                setState(() {
-                  language = value;
-                  showSnackBar();
-                });
-              },
-            ),
-            title: (Text('Kotlin')),
-          ),
-          ListTile(
-            leading: Radio<String>(
-              value: 'Swift',
-              groupValue: language,
-              onChanged: (String value){
-                setState(() {
-                  language = value;
-                  showSnackBar();
-                });
-              },
-            ),
-            title: (Text('Swift')),
-          )
-        ],
+      body: ListTile(
+        leading: Checkbox(
+          value: isAgree,
+          onChanged: (bool value){
+            setState(() {
+              isAgree = value;
+            });
+          },
+        ),
+        title: Text('Agree/Disagree'),
       )
-    );
-  }
-
-  void showSnackBar(){
-    ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(language),
-          duration: Duration(seconds: 1),
-        )
     );
   }
 }
