1f454ab5f162842698136b785caca0b7dbc91fab
bayazid_sustami <bayazidsustamy@gmail.com>
2021-06-20 13:15:23 +0800

    update homeUI

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/model/meals_model.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -31,7 +32,7 @@ class MainScreen extends StatelessWidget {
                       builder: (BuildContext context,  BoxConstraints constraints){
                         if(constraints.maxWidth <= 600){
                           return ItemGrid(countGridItem: 2);
-                        } else if(constraints.maxWidth <= 1200){
+                        } else if(constraints.maxWidth <= 900){
                           return ItemGrid(countGridItem: 4);
                         } else{
                           return ItemGrid(countGridItem: 6);
@@ -57,70 +58,72 @@ class ItemGrid extends StatelessWidget{
     return StaggeredGridView.countBuilder(
         crossAxisSpacing: 10,
         mainAxisSpacing: 12,
-        itemCount: 20,
+        itemCount: listMeal.length,
         crossAxisCount: countGridItem,
-        itemBuilder: (context, index) =>
-            MainItem(mealName: 'Ikan'),
-        staggeredTileBuilder: (count) =>
-            StaggeredTile.count(1, count.isEven ? 1.2 : 1.4)
-    );
-  }
-}
-
-class MainItem extends StatelessWidget {
-  final String mealName;
-
-  MainItem({required this.mealName});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Expanded(
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: FadeInImage.memoryNetwork(
-                      placeholder: kTransparentImage,
-                      image: 'https://www.themealdb.com/images/media/meals/1548772327.jpg',
-                      fit: BoxFit.cover,
-                  )
-              ),
-          ),
-          SizedBox(height: 7,),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                  flex: 3,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+        itemBuilder: (context, index){
+          return InkWell(
+            onTap: (){},
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: FadeInImage.memoryNetwork(
+                          placeholder: kTransparentImage,
+                          image: listMeal[index].mealImg,
+                          fit: BoxFit.cover,
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 7,),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.blueGrey,
+                      Expanded(
+                        child: Text(listMeal[index].mealName, style: TextStyle(fontFamily: 'Oxygen', fontWeight: FontWeight.bold),),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 7,),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.favorite,
+                                color: Colors.blueGrey,
+                              ),
+                              Text(
+                                listMeal[index].mealLikeCount,
+                                style: TextStyle(
+                                    fontFamily: 'Oxygen',
+                                    color: Colors.blueGrey
+                                ),
+                              )
+                            ],
+                          )
                       ),
-                      Text(
-                        '1200',
-                        style: TextStyle(
-                          fontFamily: 'Oxygen',
-                          color: Colors.blueGrey
-                        ),
+                      Expanded(
+                          flex: 1,
+                          child: BookmarkButton()
                       )
                     ],
                   )
+                ],
               ),
-              Expanded(
-                  flex: 1,
-                  child: BookmarkButton()
-              )
-            ],
-          )
-        ],
-      ),
+            ),
+          );
+        },
+        staggeredTileBuilder: (count) =>
+            StaggeredTile.count(1, count.isEven ? 1.2 : 1.4)
     );
   }
 }
--- lib/model/meals_model.dart
+++ lib/model/meals_model.dart
@@ -1,3 +1,89 @@
 class MealModel{
+  String mealName;
+  String mealId;
 
+  MealModel({
+    required this.mealId,
+    required this.mealName,
+    required this.mealImg,
+    required this.mealLikeCount});
+
+  String mealImg;
+  String mealLikeCount;
 }
+
+var listMeal = [
+  MealModel(
+      mealId: "52928",
+      mealName: "BeaverTails",
+      mealImg: "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg",
+      mealLikeCount: "2304"
+  ),
+  MealModel(
+      mealId: "52965",
+      mealName: "Breakfast Potatoes",
+      mealImg: "https://www.themealdb.com/images/media/meals/1550441882.jpg",
+      mealLikeCount: "1043"
+  ),
+  MealModel(
+      mealId: "52923",
+      mealName: "Canadian Butter Tarts",
+      mealImg: "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg",
+      mealLikeCount: "1243"
+  ),
+  MealModel(
+      mealId: "52924",
+      mealName: "Nanaimo Bars",
+      mealImg: "https://www.themealdb.com/images/media/meals/vwuprt1511813703.jpg",
+      mealLikeCount: "1330"
+  ),
+  MealModel(
+      mealId: "52930",
+      mealName: "Pate Chinois",
+      mealImg: "https://www.themealdb.com/images/media/meals/yyrrxr1511816289.jpg",
+      mealLikeCount: "1330"
+  ),
+  MealModel(
+      mealId: "52932",
+      mealName: "Pouding chomeur",
+      mealImg: "https://www.themealdb.com/images/media/meals/yqqqwu1511816912.jpg",
+      mealLikeCount: "12330"
+  ),
+  MealModel(
+      mealId: "52804",
+      mealName: "Poutine",
+      mealImg: "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
+      mealLikeCount: "1332"
+  ),
+  MealModel(
+      mealId: "52933",
+      mealName: "Rappie Pie",
+      mealImg: "https://www.themealdb.com/images/media/meals/ruwpww1511817242.jpg",
+      mealLikeCount: "13431"
+  ),
+  MealModel(
+      mealId: "52925",
+      mealName: "Split Pea Soup",
+      mealImg: "https://www.themealdb.com/images/media/meals/xxtsvx1511814083.jpg",
+      mealLikeCount: "52925"
+  ),
+  MealModel(
+      mealId: "52931",
+      mealName: "Sugar Pie",
+      mealImg: "https://www.themealdb.com/images/media/meals/yrstur1511816601.jpg",
+      mealLikeCount: "32309"
+  ),
+  MealModel(
+      mealId: "52929",
+      mealName: "Timbits",
+      mealImg: "https://www.themealdb.com/images/media/meals/txsupu1511815755.jpg",
+      mealLikeCount: "21039"
+  ),
+  MealModel(
+      mealId: "52926",
+      mealName: "Tourtiere",
+      mealImg: "https://www.themealdb.com/images/media/meals/ytpstt1511814614.jpg",
+      mealLikeCount: "32102"
+  ),
+
+];
