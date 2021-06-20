e195454866dad5fc8f11a32966b67caf7a271933
bayazid_sustami <bayazidsustamy@gmail.com>
2021-06-21 03:31:20 +0800

    update dynamic content

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/model/detail_meals_model.dart';
 import 'package:html_unescape/html_unescape.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -30,7 +31,7 @@ class DetailScreen extends StatelessWidget{
                           ),
                           child : FadeInImage.memoryNetwork(
                               placeholder: kTransparentImage,
-                              image: 'https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg')
+                              image: getDetail(idMeal).mealImg)
                       ),
                       Padding(
                         padding: EdgeInsets.all(16),
@@ -60,7 +61,7 @@ class DetailScreen extends StatelessWidget{
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         Text(
-                          'BeaverTails',
+                          getDetail(idMeal).mealName,
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 24,
@@ -69,7 +70,7 @@ class DetailScreen extends StatelessWidget{
                         ),
                         SizedBox(height: 7,),
                         Text(
-                          'Dessert',
+                          getDetail(idMeal).mealCategory,
                           style: TextStyle(
                             fontSize: 16,
                             fontFamily: 'Oxygen',
@@ -90,7 +91,7 @@ class DetailScreen extends StatelessWidget{
                               ),
                               SizedBox(height: 10,),
                               Text(
-                                unescape.convert('In the bowl of a stand mixer, add warm water, a big pinch of sugar and yeast. Allow to sit until frothy.\r\nInto the same bowl, add 1/2 cup sugar, warm milk, melted butter, eggs and salt, and whisk until combined.\r\nPlace a dough hook on the mixer, add the flour with the machine on, until a smooth but slightly sticky dough forms.\r\nPlace dough in a bowl, cover with plastic wrap, and allow to proof for 1 1/2 hours.\r\nCut dough into 12 pieces, and roll out into long oval-like shapes about 1/4 inch thick that resemble a beaver’s tail.\r\nIn a large, deep pot, heat oil to 350 degrees. Gently place beavertail dough into hot oil and cook for 30 to 45 seconds on each side until golden brown.\r\nDrain on paper towels, and garnish as desired. Toss in cinnamon sugar, in white sugar with a squeeze of lemon, or with a generous slathering of Nutella and a handful of toasted almonds. Enjoy!'),
+                                unescape.convert(getDetail(idMeal).instructions),
                                 style: TextStyle(
                                   fontSize: 16,
                                   fontFamily: 'Oxygen',
@@ -109,6 +110,15 @@ class DetailScreen extends StatelessWidget{
         )
       )
     );
+  }
+
+  DetailMealsModel getDetail(String idMeal){
+    for(var items in listDetailMeal){
+      if(items.idMeal == idMeal){
+        return items;
+      }
+    }
+    throw new Exception('unexpected id');
   }
 }
 
